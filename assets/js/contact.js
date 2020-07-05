7675873ef676f5f652b4e3ac84ba4bc5978a77fc
ichoi21 <ihchoi@gmail.com>
2020-07-05 13:37:41 -0700

    contact section complete

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,28 +2,27 @@ $("contact").append(
   `
   <div class="container">
     <div class="row text-center">
-      <div class="col-sm-12 mt-5 p-5 shadow-lg">
+      <div class="col-sm-12 mt-5 p-2">
       <ul class="list-inline">
-      <i class="icon-social-facebook"></i>
-    </a>
-  </li>
-  <li class="list-inline-item">
-    <a
-      class="social-link rounded-circle text-white mr-3"
-      href="https://www.linkedin.com/in/ichoi21/"
-    >
-      <i class="icon-social-linkedin">Linkedin</i>
-    </a>
-  </li>
-  <li class="list-inline-item">
-    <a
-      class="social-link rounded-circle text-white"
-      href="https://github.com/ichoi21"
-    >
-      <i class="icon-social-github">GitHub</i>
-    </a>
-  </li>
-</ul>
+     
+      <li class="list-inline-item">
+        <a class="contact-logo rounded-circle text-white mr-3"
+          href="https://www.linkedin.com/in/ichoi21/" target="blank">
+          <i class="fab fa-linkedin-in"></i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+      <a class="contact-link rounded-circle text-white mr-3"
+        href="https://www.illiest.com" target="blank">il
+      </a>
+    </li> 
+      <li class="list-inline-item">
+        <a class="contact-logo rounded-circle text-white"
+          href="https://github.com/ichoi21" target="blank">
+          <i class="fab fa-github"></i>
+        </a>
+      </li>
+      </ul>
       </div> 
     </div>
   </div>
