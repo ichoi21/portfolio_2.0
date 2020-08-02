5faeb0f6974fe6013e75157d95a6a2706d7db39b
ichoi21 <ihchoi@gmail.com>
2020-08-01 18:23:15 -0700

    contact materialized

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,33 +2,24 @@ $(document).on("click", "#btnC", function (e) {
   e.preventDefault();
   $("article").html(
     `
-  <div class="container z-depth-4" id="contact">
-    <div class="row center-align">
-      <div class="col-sm-12">
-    <h5 class="text-secondary center-align indigo">connect w. ian</h5>
-      </div>
-      </div>
-    <div class="row center-align">
-      <div class="col-sm-12 pb-2">
-      <ul class="list-inline">
-        <li class="list-inline-item hoverable circle">
-        <a class="contact-logo rounded-circle white-text"
-          href="https://www.linkedin.com/in/ichoi21/" target="blank" alt="Linkedin">
-          <i class="fab fa-linkedin-in"></i>
+  <div class="container z-depth-4 white-text center-align" id="contact" style="padding: 25px;">
+    <div class="row">
+      <span class="badge teal hoverable" style="font-size: 24px;">connect w. ian</span>
+      </br>
+      </br>
+      If you're interested in connecting with ian, below are links where you can go to reach ian.
+    </div>
+    <div class="nav-wrapper">
+      <ul class="center">
+        <a class=" btn-floating pulse teal-text hoverable btn-large" href="https://www.linkedin.com/in/ichoi21/" target="blank" alt="Linkedin"><i class="fab fa-linkedin-in"></i>
         </a>
-        </li>
-        <li class="list-inline-item hoverable circle">
-        <a class="contact-link rounded-circle white-text text-center" href="https://www.illiest.com" target="blank">il
+        &nbsp;          &nbsp;          &nbsp;
+        <a class="btn-floating pulse contact-link white-text no-uppercase center-align btn-large hoverable" href="https://www.illiest.com" target="blank">il
         </a>
-        </li> 
-        <li class="list-inline-item hoverable circle">
-        <a class="contact-logo rounded-circle white-text"
-          href="https://github.com/ichoi21" target="blank">
-          <i class="fab fa-github"></i>
+        &nbsp;          &nbsp;          &nbsp;
+        <a class="btn-floating pulse teal-text hoverable btn-large" href="https://github.com/ichoi21" target="blank"><i class="fab fa-github" style="font-size:52px;"></i>
         </a>
-        </li>
       </ul>
-      </div> 
     </div>
   </div>
 `
